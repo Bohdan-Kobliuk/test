9fa17b613b9df71b5af7318269547de71535b622
Bohdan-Kobliuk <92303470+Bohdan-Kobliuk@users.noreply.github.com>
2021-12-06 20:09:39 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,22 +3,4 @@ resource "aws_instance" "web1" {
   instance_type = "t2.micro"
 
 }
-resource "aws_security_group" "allow_http" {
-  name        = "allow_http"
 
-  ingress {
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-}
