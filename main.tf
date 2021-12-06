7b6f8244d672b8ff38ca867404416b7fb314b02c
Bohdan-Kobliuk <92303470+Bohdan-Kobliuk@users.noreply.github.com>
2021-12-06 19:57:06 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,8 +11,7 @@ module "vpc" {
   name                 = "task"
   cidr                 = "10.0.0.0/16"
   azs                  = ["us-east-1a","us-east-1b"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24"]
-  private_subnets = ["10.0.6.0/24"]
+  public_subnets       = ["10.0.4.0/24"]
   enable_dns_hostnames = true
   enable_dns_support   = true
 }
